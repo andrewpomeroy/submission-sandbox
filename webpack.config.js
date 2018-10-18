5bdc9c5241b74bda6302daf090e96bf2adf6ab7c
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2018-10-18 12:44:32 -0700

    Revert "some webpack config changes?"
    This reverts commit fd1b8a16bb04054dd96c251ef4b85c03cb4edd91.

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,66 @@
-/* eslint-env node */
-
-// const webpack = require('webpack');
+const webpack = require('webpack');
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-	context: path.join(__dirname, "./"),
-	entry: path.join(__dirname, "./public/scripts/main.js"),
-	devtool: "source-map",
-	mode: "development",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js",
-	},
-	resolve: {
-		extensions: [".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				loader: "babel-loader",
-				exclude: /node_modules/,
-				include: path.join(__dirname, "./public/scripts"),
-			},
-
-			{
-      	test:/\.(s*)css$/,      
-				use:["style-loader","css-loader", "sass-loader"],
-			},
+  context: path.join(__dirname, './'),
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src'),
+      },
       // {
-
-			{
-				test: /\.html/,
-				use: "raw-loader"
-			}
-		],
-	},
-	watch: true
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader']
+      // }
+      {
+        test:/\.(s*)css$/,
+        use:['style-loader','css-loader', 'sass-loader'],
+      },
+      // {
+      //   test:/\.(s*)css$/,
+      //   exclude: [/node_modules/],
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: ['css-loader', 'sass-loader'],
+      //   }),
+      // },
+      
+      // {
+      //   test: /.css$/,
+      //   exclude: [/node_modules/],
+      //   use: vendorsExtractPlugin.extract({
+      //     use: ['css-loader'],
+      //   }),
+      // },
+      // { test: /\.html$/, loader: 'ng-cache?prefix=[dir]/[dir]' },
+      {
+        test: /\.html/,
+        use: 'raw-loader'
+      }
+    ],
+  },
+  watch: true
   // devtool: 'source-map',
   // plugins: [
   //   new ExtractTextPlugin({
   //     filename: 'bundle.css',
   //     allChunks: true
   //   }),
-  // new webpack.DefinePlugin({
-  //   SOCKET_URL: JSON.stringify(process.env.SOCKET_URL ? process.env.SOCKET_URL : 'wss://localhost:3000'),
-  // }),
+    // new webpack.DefinePlugin({
+    //   SOCKET_URL: JSON.stringify(process.env.SOCKET_URL ? process.env.SOCKET_URL : 'wss://localhost:3000'),
+    // }),
   // ],
 };
