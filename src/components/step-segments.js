ad690c04e14c08edf252673601787836375e3e51
Andrew Pomeroy <andrew_pomeroy@windsorsolutions.com>
2018-11-15 14:36:02 -0800

    convert step-segments to click-based versus hover-based

--- src/components/step-segments.js
+++ src/components/step-segments.js
@@ -18,51 +18,56 @@ function StepSegmentsController($timeout) {
 
 	Object.defineProperties(this, {
 		activeIndex: {
+			get: () => this._activeIndex || this.initialActiveIndex || 0,
+			set: (value) => this._activeIndex = value
+		},
+		hoveredIndex: {
 			get: () => {
-				return (this._activeIndex !== undefined) ? this._activeIndex : this.hardActiveIndex;
+				return (this._hoveredIndex !== undefined) ? this._hoveredIndex : this.activeIndex;
 			},
 			set: (value) => {
-				if (value && value === this._activeIndex && this.activationTimeout) {
+				if (value && value === this._hoveredIndex && this.activationTimeout) {
 					$timeout.cancel(this.activationTimeout);
 					this.activationTimeout = null;
 				}
 
-				if (value !== this._activeIndex) {
+				if (value !== this._hoveredIndex) {
 					// console.log('activationTimeout', this.activationTimeout, value);
 					if (this.activationTimeout) {
 						// console.log('CLEARING', this.activationTimeout);
 						$timeout.cancel(this.activationTimeout);
 						this.activationTimeout = null;
 					}
-					this.activationTimeout = $timeout(() => this._activeIndex = value, 50);
+					this.activationTimeout = $timeout(() => this._hoveredIndex = value, 50);
 					// console.log('setting new activationTimeout', this.activationTimeout, 'for value', value);
 				}
 			}
 		},
 	});
 
-	this.setActivatedIndex = (value) => {
-		this.activeIndex = value;
+	this.setHoveredIndex = (value) => {
+		this.hoveredIndex = value;
 	};
 
 	this.softActivate = value => {
-		this.setActivatedIndex(value);
+		this.setHoveredIndex(value);
 	};
 	this.softDeactivate = () => {
-		this.setActivatedIndex(undefined);
+		this.setHoveredIndex(undefined);
 	};
 	this.hardActivate = value => {
-		this.setActivatedIndex(value);
-		this.hardActiveIndex = value;
+		this.setHoveredIndex(value);
+		this.activeIndex = value;
 	};
 
+	this.onClick = index => this.hardActivate(index);
 	this.onMouseEnter = index => this.softActivate(index);
 	this.onMouseLeave = () => this.softDeactivate();
 	this.onFocus = index => this.softActivate(index);
 	this.onBlur = () => this.softDeactivate();
 
 	this.$onInit = function () {
-		this.hardActiveIndex = this.initialActiveIndex;
+		this.activeIndex = this.initialActiveIndex;
 	};
 }
 
