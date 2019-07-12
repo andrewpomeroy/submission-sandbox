a64e999067e5a09bd0e5e55f7e3a5298b2465855
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2019-07-12 16:44:45 -0700

    begin split webpack config tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+// const merge = require('webpack-merge');
+// const common = require('./webpack.common.js');
+
+console.log("ENV", process.env.NODE_ENV);
 
 module.exports = {
   context: path.join(__dirname, './'),
@@ -20,10 +27,23 @@ module.exports = {
         exclude: /node_modules/,
         include: path.join(__dirname, 'src'),
       },
+      // {
+      //   test:/\.scss$/,
+      //   use:['style-loader','css-loader', 'sass-loader'],
+      // },      
       {
-        test:/\.(s*)css$/,
-        use:['style-loader','css-loader', 'sass-loader'],
-      },      
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
       {
         test: /\.html/,
         use: 'raw-loader'
@@ -46,5 +66,14 @@ module.exports = {
       }
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+  ],
   watch: true
+
 };
