c2fd3e110875b8c2b9656ed968455e1f8c3f10f2
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2019-08-12 21:55:07 -0700

    add big modal select menu to Reassign Step

--- src/components/step-menu.js
+++ src/components/step-menu.js
@@ -1,4 +1,5 @@
 import template from "./step-menu.html";
+import faker from "faker";
 
 const StepMenu = {
   template: template,
@@ -8,12 +9,27 @@ const StepMenu = {
   controller: StepMenuController
 };
 
-StepMenuController.$inject = [];
+StepMenuController.$inject = ["$mdDialog"];
 
-function StepMenuController() {
+function StepMenuController($mdDialog) {
   var $ctrl = this;
+  $ctrl.reassignDialogId = "reassignDialog" + Math.floor(Math.random() * 1000);
+
+  this.reassign = function ($event, step) {
+    $mdDialog.show({
+      contentElement: "#" + $ctrl.reassignDialogId,
+      // parent: angular.element(document.body),
+      targetEvent: $event,
+      clickOutsideToClose: true,
+    });
+  };
+
 
   this.$onInit = function() {
+    $ctrl.users = [];
+    for (let index = 0; index < (Math.floor(Math.random() * 15) + 5); index++) {
+      $ctrl.users.push({ username: faker.name.findName() });
+    }
   }; 
 }
 
