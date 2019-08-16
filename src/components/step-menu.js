fb49eda20e40e8456e399e97dcff4463812f9306
Andrew Pomeroy <andrew_pomeroy@windsorsolutions.com>
2019-08-16 15:55:01 -0700

    fix resetting of reassign dialog

--- src/components/step-menu.js
+++ src/components/step-menu.js
@@ -5,6 +5,9 @@ const StepMenu = {
   template: template,
   bindings: {
     step: "<",
+    onReassign: "<",
+    onDelete: "<",
+    onToggleComplete: "<"
   },
   controller: StepMenuController
 };
@@ -12,7 +15,19 @@ const StepMenu = {
 function StepMenuController() {
   var $ctrl = this;
 
-  this.reassign = function (step, $event) {
+  this.openReassignDialog = function (step, $event) {
+    $ctrl.reassignActive = true;
+    $ctrl.$reassignEvent = $event;
+  };
+
+  this.reassign = (user) => {
+    $ctrl.reassignActive = false;
+  };
+
+  this.reassignUserItemDisplayFn = (item) => item.displayName;
+  this.reassignUserIsSelectedFn = (user) => user.isAssigned;
+
+  this.$onInit = function() {
     $ctrl.users = [];
     // create fake "users" list
     for (let index = 0; index < (Math.floor(Math.random() * 20) + 50); index++) {
@@ -21,21 +36,6 @@ function StepMenuController() {
     // Mock effect of designating one user as current assignee
     const int = Math.floor(Math.random() * $ctrl.users.length);
     $ctrl.users[int].isAssigned = true;
-
-    $ctrl.reassignActive = true;
-    $ctrl.$reassignEvent = $event;
-  };
-
-  this.onReassign = (user) => {
-    $ctrl.reassignActive = false;
-    console.log(user, $ctrl.step);
-  };
-
-  this.reassignUserItemDisplayFn = (item) => item.displayName;
-  this.reassignUserIsSelectedFn = (user) => user.isAssigned;
-
-  this.$onInit = function() {
-
   };
 
 }
